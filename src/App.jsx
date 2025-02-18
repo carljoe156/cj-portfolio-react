464f81e26d8e2fa390ecc155410b1b098f81e5ed
Carl Joseph <carljoseph156@gmail.com>
2025-02-18 12:48:21 -0500

    Added/Updated Profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Profile from "./components/Profile";
 import Footer from "./components/Footer";
-
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Profile />
       <Footer />
     </div>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <section id="profile">
+      <div className="section__pic-container">
+        <img
+          src="./images/Carl-Joseph-profile-picture.jpg"
+          alt="Carl Joseph profile picture"
+        />
+      </div>
+      <div className="section__text">
+        <p className="section__text__p1">Hello, I'm</p>
+        <h1 className="title">Carl Joseph</h1>
+        <p className="section__text__p2">
+          Frontend Developer, based in Brooklyn New York
+        </p>
+        <div className="btn-container">
+          <button
+            className="btn btn-color-2"
+            onClick={() => window.open("./assets/resume-example.pdf")}
+          >
+            Download CV
+          </button>
+          <button
+            className="btn btn-color-1"
+            onClick={() => (location.href = "./#contact")}
+          >
+            Contact Info
+          </button>
+        </div>
+        <div id="socials-container">
+          <img
+            src="/images/linkedin.png"
+            alt="My LinkedIn profile"
+            className="icon"
+            onClick={() => (location.href = "https://linkedin.com/")}
+          />
+          <img
+            src="/images/github.png"
+            alt="My Github profile"
+            className="icon"
+            onClick={() => (location.href = "https://github.com/")}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Profile;
