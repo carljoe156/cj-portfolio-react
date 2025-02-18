794065afb8b2fae68b72c60aee0e2101913561b1
Carl Joseph <carljoseph156@gmail.com>
2025-02-18 11:41:14 -0500

    Added/Updated Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; 2025 Carl Joseph. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
