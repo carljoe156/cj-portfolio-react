44e4d0b1cccb02ba9651d1afe8044992f180e9ee
Carl Joseph <carljoseph156@gmail.com>
2025-03-03 19:20:20 -0500

    Added/Updated Data

---
+++ src/data/projectData.js
@@ -0,0 +1,44 @@
+const projects = [
+  {
+    id: 1,
+    title: "VIRTUAL MET MUSEUM",
+    description:
+      "A Virtual visualization of the MET MUSEUM built using React and the MET API.",
+    liveDemo: "https://cozy-brigadeiros-8e017f.netlify.app/",
+    github: "https://github.com/carljoe156/SBA-320H-React-Web-Application.git",
+    images: [
+      "/images/VirtualMet-Homepage_Featured_Random_Artwork.png",
+      "/images/VirtualMet-HomePage_Our_Collection.png",
+      "/images/VirtualMet-Departments_Page_pt1.png",
+      "/images/VirtualMet-European_Department_Artworks_pt2.png",
+      "/images/VirtualMet-Random_Artwork_Page.png",
+      "/images/VirtualMet-Random_Artwork_Page_2.png",
+    ],
+  },
+  {
+    id: 2,
+    title: "Poetically-BookList",
+    description: "A simple book management app built with MERN.",
+    liveDemo:
+      "https://github.com/carljoe156/Joseph_Carl_Poetically_Capstone_Frontend.git",
+    github:
+      "https://github.com/carljoe156/Joseph_Carl_Poetically_Capstone_Frontend.git",
+    images: [
+      "/images/Poetically-Home.png",
+      "/images/Poetically-BookDetails.png",
+      "/images/Poetically-About.png",
+      "/images/Poetically-Discover,Blog.png",
+      "/images/Poetically-Lists,Registry.png",
+    ],
+  },
+  {
+    id: 3,
+    title: "Weather App",
+    description: "Weather App made with HTML, CSS, and JavaScript.",
+    liveDemo: "https://github.com/carljoe156/SBA-Module-308A-JS.git",
+    github: "https://github.com/carljoe156/SBA-Module-308A-JS.git",
+    images: ["/images/WeatherStar-Live.png"],
+  },
+];
+
+export default projects;
