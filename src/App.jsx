36ea5b41326f3392ee6899a1a85c008ca7a60a43
Carl Joseph <carljoseph156@gmail.com>
2025-03-29 22:11:48 -0400

    Updated main app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import ProjectDetails from "./components/ProjectDetails";
+import Blog from "./pages/Blog";
 import "./App.css";
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
       </Routes>
       <Footer />
     </>
